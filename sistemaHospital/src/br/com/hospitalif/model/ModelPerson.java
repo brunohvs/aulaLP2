e7d0d547bdb9b67d40689e94b2a16ad833368427
bruno <brunohvs3000@gmail.com>
2019-08-22 19:40:33 -0400

    ...

--- sistemaHospital/src/br/com/hospitalif/model/ModelPerson.java
+++ sistemaHospital/src/br/com/hospitalif/model/ModelPerson.java
@@ -1,5 +1,49 @@
 package br.com.hospitalif.model;
 
 public class ModelPerson {
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.control.ChoiceBox;
+	import javafx.scene.control.DatePicker;
+	import javafx.scene.control.TextField;
+
+	public class HospitalControl {
+
+	    @FXML
+	    private TextField txtName;
+
+	    @FXML
+	    private TextField txtCPF;
+
+	    @FXML
+	    private DatePicker pckBirthDate;
+
+	    @FXML
+	    private ChoiceBox<?> cboBlood;
+
+	    @FXML
+	    private ChoiceBox<?> cboSex;
+
+	    @FXML
+	    private ChoiceBox<?> cboStatus;
+
+	    @FXML
+	    private Button btnBack;
+
+	    @FXML
+	    private Button btnOK;
+
+	    @FXML
+	    void setCpf(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void setName(ActionEvent event) {
+
+	    }
+
+	}
+
 
 }
